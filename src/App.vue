f5fd49141bee4770fd47243fa9e081acf04ea676
LyleBrown010 <lylebrown010@gmail.com>
2023-07-03 12:39:44 +0200

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <NavBarComp/>
+    <router-view/>
+    <FooterComp/>
 </template>
-
+<script>
+import NavBarComp from './components/NavBar-Comp.vue';
+import FooterComp from './components/Footer-Comp.vue';
+export default {
+  components: {NavBarComp, FooterComp}
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,16 +19,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+*{
+  background-color: #F8F6DD;
 }
 </style>
