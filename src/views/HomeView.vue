1afc55d7801920c16d05ef7732498440c0c4c742
LyleBrown010 <lylebrown010@gmail.com>
2023-07-03 15:27:48 +0200

    homr start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <p>HIHIHI</p>
+    <p>HIHIHIH1</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
