c8bf06450e80bfec46945bb62e4ff7e4680e70de
LyleBrown010 <lylebrown010@gmail.com>
2023-07-04 19:17:04 +0200

    structure of code done

--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,12 +1,23 @@
 <template>
+    <div v-if="testimonials" class="flex-container" id="testimonials">
+        <CardComp v-for="testimonial of testimonials" :key="testimonial.id" :testimonial="testimonial"/>
+    </div>
+    <div v-else> Loading ... </div>
 </template>
 <script>
 import CardComp from '@/components/Testimonial-Card.vue'
 
 export default {
-    data(){
-        
-    }
+    computed: {
+        testimonials (){
+            return this.$store.state.testimonials
+        }
+    },
+    mounted(){
+        this.$store.dispatch("getTestimonials")
+    },
+
+    components:{CardComp}
 
 }
 </script>
