82aca8d597950e9570ee7226a1d537776b57d839
LyleBrown010 <lylebrown010@gmail.com>
2023-07-05 15:21:11 +0200

    layout done

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,54 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    education: null, 
+    educations: null, 
+    experiences: null,
+    skills: null, 
+    softSkills: null,
   },
   mutations: {
+    setEducations: (state, educations) => {
+      state.educations = educations;
+    },
+    
+    setExperiences: (state, experiences) => {
+      state.experiences = experiences;
+    },
+
+    setSkills: (state, skills) => {
+      state.skills = skills; 
+    }, 
+
+    setsoftSkills: (state, softSkills) => {
+      state.softSkills = softSkills; 
+    }
   },
   actions: {
-  },
+    getEducations: async (context) => {
+      fetch("https://lylebrown010.github.io/Data/education.json")
+      .then((res) => res.json())
+      .then((educations) => context.commit("setEducations", educations));
+    },
+
+    getExperiences: async (context) => {
+      fetch("https://lylebrown010.github.io/Data/experience.json")
+      .then((res) => res.json())
+      .then((experiences) => context.commit("setExperiences", experiences));
+    }, 
+
+    getSkills: async (context) => {
+      fetch("https://lylebrown010.github.io/Data/skills.json")
+      .then((res) => res.json())
+      .then((skills) => context.commit("setSkills", skills));
+    },
+
+    getsoftSkills: async (context) => {
+      fetch("https://lylebrown010.github.io/Data/softSkills.json")
+      .then((res) => res.json())
+      .then((softSkills) => context.commit("setsoftSkills", softSkills));
+    }
+
+
+
+  }, 
 })
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,1 +1,58 @@
-<template>This is the resume Page...</template>
+<template>
+<div v-if="educations" class="flex-container">
+    <educationCardComp v-for="education of educations" :key="education.id" :education="education"/> 
+</div>
+<div v-else>Loading...</div>
+
+<div v-if="experiences" class="flex-container">
+    <experienceCardComp v-for="experience of experiences" :key="experience.id" :experience="experience"/> 
+</div>
+<div v-else>Loading...</div>
+
+<div v-if="skills" class="flex-container">
+    <skillCardComp v-for="skill of skills" :key="skill.id" :skill="skill"/> 
+</div>
+<div v-else>Loading...</div>
+
+<div v-if="softSkills" class="flex-container">
+    <softSkillCardComp v-for="softSkill of softSkills" :key="softSkill.id" :softSkill="softSkill"/> 
+</div>
+<div v-else>Loading...</div>
+
+
+
+</template>
+
+<script>
+import educationCardComp from '@/components/Education-Comp.vue'
+import experienceCardComp from '@/components/Experience-Comp.vue'
+import skillCardComp from '@/components/Skills-Comp.vue'
+import softSkillCardComp from '@/components/SoftSkills-Comp.vue'
+
+export default {
+    computed:{
+        educations(){
+            return this.$store.state.educations
+        },
+        experiences(){
+            return this.$store.state.experiences
+        },
+        skills(){
+            return this.$store.state.skills
+        },
+        softSkills(){
+            return this.$store.state.softskills
+        }
+    },
+    
+    mounted(){
+        this.$store.dispatch("getEducations")
+        this.$store.dispatch("getExperiences")
+        this.$store.dispatch("getSkills")
+        this.$store.dispatch("getsoftSkills")
+
+    }, 
+
+    components:{educationCardComp, experienceCardComp, skillCardComp, softSkillCardComp}
+}
+</script>
