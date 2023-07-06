b82047104c0849ce790e07f268cbc3491ee0624f
LyleBrown010 <lylebrown010@gmail.com>
2023-07-07 00:33:21 +0200

    added clip art

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
 <br><br>
-<div class="myName">LYLE BROWN.</div>
+<div class="top">
+  <img src="https://i.postimg.cc/Wbxm4yXd/Colorful-Retro-Creative-Portfolio-Presentation-removebg-preview.png" alt="">
+<h1 class="myName">LYLE BROWN.</h1>
+</div>
+
+<div ></div>
 <div class="myIntro">Aspiring Full Stack Developer.</div>
 <br><br>
 
 </template>
 <style scoped>
+.top{
+  display: flex;
+}
+img{
+  width: 10%;
+  height: 10%;
+  display: flex;
+  margin-left: 5%;
+}
 .myName{
   font-size: 100px;
   font-family: 'Caprasimo', cursive;
