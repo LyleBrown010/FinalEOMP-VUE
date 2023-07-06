d63d241e9b4d4c4c1ff63966f819d94b140be6b2
LyleBrown010 <lylebrown010@gmail.com>
2023-07-06 23:54:37 +0200

    changed font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 <br><br>
-<div class="myName">Lyle Brown.</div>
+<div class="myName">LYLE BROWN.</div>
 <div class="myIntro">Aspiring Full Stack Developer.</div>
 <br><br>
 
@@ -8,13 +8,13 @@
 <style scoped>
 .myName{
   font-size: 100px;
-  font-family: 'Bungee', cursive;
+  font-family: 'Caprasimo', cursive;
   color: black;
 }
 
 .myIntro{
   font-size: 40px;
-  font-family: 'Bungee', cursive;
+  font-family: 'Caprasimo', cursive;
   color: black;
 }
 </style>
