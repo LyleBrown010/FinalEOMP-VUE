d44daa319ea8d2b794f3da56e910229b924faab6
LyleBrown010 <lylebrown010@gmail.com>
2023-07-06 20:08:01 +0200

    tarted styling

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,12 +2,12 @@
     <div v-if="projects" class="flex-container">
         <div v-for="project of projects" :key="project.name">
             <div class="card">
-                <div>
+                <div id="links">
                 <a :href="project.netlify" target="_blank"><button>NETLIFY</button></a>
                 <a :href="project.github" target="_blank"><button>GITHUB</button></a>
             </div>
             <strong>{{project.name}}</strong>
-            <img :src="project.image" :alt="project.name">
+            <img id="image" :src="project.image" :alt="project.name">
             <p>{{project.description}}</p>
             </div>
         </div>
@@ -31,3 +31,26 @@ export default {
     // components: {projectCardComp}
 }
 </script>
+
+<style scoped>
+
+.card{
+    width: 50%;
+    margin: 1%;
+    border: 2px solid black;
+    border-radius: 15px;
+    padding: 1%;
+    
+}
+a{
+    margin: 1%;
+}
+
+#links{
+    border-radius: 15px;
+}
+
+#image{
+    width: 100%;
+}
+</style>
