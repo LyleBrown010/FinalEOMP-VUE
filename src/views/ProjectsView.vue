2bc17192d51713d4802f6ac657aae8d0f0d599a7
LyleBrown010 <lylebrown010@gmail.com>
2023-07-06 16:28:14 +0200

    added try and catch

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,13 +1,15 @@
 <template>
     <div v-if="projects" class="flex-container">
         <div v-for="project of projects" :key="project.name">
-            <div>
-                <a href="{{project.netlify}}"><button>NETLIFY</button></a>
-                <a href="{{project.github}}"><button>GITHUB</button></a>
+            <div class="card">
+                <div>
+                <a :href="project.netlify" target="_blank"><button>NETLIFY</button></a>
+                <a :href="project.github" target="_blank"><button>GITHUB</button></a>
             </div>
             <strong>{{project.name}}</strong>
             <img :src="project.image" :alt="project.name">
             <p>{{project.description}}</p>
+            </div>
         </div>
     </div>
     <div v-else>Loading .........</div>
