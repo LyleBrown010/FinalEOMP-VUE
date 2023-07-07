537eacf7f9542cdc3a8ff016dbb320d96541e410
LyleBrown010 <lylebrown010@gmail.com>
2023-07-07 11:13:53 +0200

    progress bar

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -33,79 +33,87 @@
             <div v-else>Loading Experience...</div>
         </div>
     </div>
+</div>
 
-
+<div class="row">
     <div class="col-md-4" id="skills">
-        <div></div>
-        <div class="block">
-            <p></p>
-        </div>
-
-
-
-        <!-- <div v-if="skills" class="flex-container">
-            <div v-for="skill of skills" :key="skill.name">
-                <div class="h-100 p-5 border border-4 border-primary rounded-3 mt-3" id="skills">
-                    <h2 class="fs-1 fw-bolder text-black">Skills</h2><br>
-                    <h4 class="text-black">{{skill.name}}</h4>
-                    <div class="progress">
-                        <div
-                            class="progress-bar"
-                            role="progressbar"
-                            style="width: 84%"
-                            aria-valuenow="25"
-                            aria-valuemin="0"
-                            aria-valuemax="100">
-                            <p class="text-black">{{skill.rating}}</p>
-                        </div>
-                    </div>
+        <div class="h-100 p-5 border border-4 border-primary rounded-3 mt-3"
+            id="skills">
+            <h2 class="fs-1 fw-bolder text-white">Skills</h2><br>
+            <h4 class="text-white">HTML</h4>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 75%;"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    <span>Advanced</span>
                 </div>
-            </div> 
-        </div>
-        <div v-else>Loading Skills...</div> -->
-    </div>
-
-    <div class="col-md-4" id="skills">
-        <div v-if="softSkills" class="flex-container">
-            <div v-for="softSkill of softSkills" :key="softSkill.name">
-                <div class="content">
-                    <div class="block">
-                        {{softSkill.name}}
-                        <br>
-                        {{softSkill.rating}}
-                    </div>
+            </div><br />
+            <h4 class="text-white">CSS</h4>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 82%"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    82%
                 </div>
-            </div>
+            </div><br />
+            <h4 class="text-white">JavaScript</h4>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 82%"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    82%
+                </div>
+            </div><br />
+            <h4 class="text-white">Vue.Js</h4>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 82%"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    82%
+                </div>
+            </div><br />
+            <h4 class="text-white">BootStrap</h4>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 83%"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    83%
+                </div>
+            </div><br /> <br>
         </div>
-            
-        
+    </div>
+
+    <div class="col-md-4" id="softSkills">
+        <div class="content">
+            <div id="block" class="block-1">Block 1</div>
+            <div id="block" class="block-2">Block 2</div>
+            <div id="block" class="block-3">Block 3</div>
+            <div id="block" class="block-4">Block 4</div>
+            <div id="block" class="block-5">Block 5</div>
+            <div id="block" class="block-6">Block 6</div>
+        </div>
     </div>
 </div>
-
-
-
-
-
-
-<div v-if="softSkills" class="flex-container">
-    <div v-for="softSkill of softSkills" :key="softSkill.name">
-        <h4 class="text-white">{{softSkill.name}}</h4>
-<div class="progress">
-    <div
-    class="progress-bar"
-    role="progressbar"
-    style="width: 84%"
-    aria-valuenow="25"
-    aria-valuemin="0"
-    aria-valuemax="100">
-    
-    </div>
-</div>
-    </div> 
-</div>
-<div v-else>Loading Soft Skills...</div>
-
-
 
 </template>
 
@@ -121,9 +129,6 @@ export default {
         },
         skills(){
             return this.$store.state.skills
-        },
-        softSkills(){
-            return this.$store.state.softSkills
         }
     },
     
@@ -131,7 +136,6 @@ export default {
         this.$store.dispatch("fetchEducations")
         this.$store.dispatch("fetchExperiences")
         this.$store.dispatch("fetchSkills")
-        this.$store.dispatch("fetchsoftSkills")
 
     }, 
 }
@@ -154,7 +158,7 @@ export default {
 .timeline-content .timeline-header, 
 .timeline-content .timeline-body {
   padding-left: 26px;
-  border-left: 3px solid blue;
+  border-left: 3px solid #45a245;
 }
 
 .timeline-body {
@@ -171,7 +175,7 @@ export default {
 .timeline-title {
   font-weight: 600;
   font-size: 20px;
-  border-bottom: 1px solid green;
+  border-bottom: 1px solid #45a245;
 }
 
 .timeline-marker {
@@ -180,29 +184,98 @@ export default {
   width: 16px;
   height: 16px;
   border-radius: 50% / 50%;
-  background: blue;
   left: -16px;
   top: 50%;
   transform: translate(50%,-50%);
+  color: #45a245;
+    border: 3px solid #45a245 ;
+    background-color: #c1fdc1;
+} 
+
+.progress-bar{
+    background-color:#c1fdc1; 
+    border: 2px solid #45a245;
+    padding: 1px;
+    border-radius: 12px;
+}
+span{
+    background-color:#c1fdc1; 
+    font-size: 12px;
+    font-weight: bold;
 }
 
 .content{
-    height: 400px;
-    width: 100%;
+    height: 200px;
+    width: 90%;
     position: relative;
     display: block;
     margin: 50px auto;
     color: #fff;
 }
 
-.block{
+#block{
     text-align: center;
-    width: 33.33%;
+    transition: all.5s ease;
+}
+
+#block:hover{
+    transform: translate3d(0, -1px, 0) scale(1.03);
+    transition: all .4s ease;
+}
+
+.block-1{
+    width: 33%;
     height: 50%;
-    background-color: aqua;
+    background-color: #45a245;
     left: 0;
     top: 0;
     position: absolute;
+    line-height: 100px;
+}
+.block-2{
+    width: 33%;
+    height: 50%;
+    background-color: #c1fdc1;
+    left: 0;
+    top: 50%;
+    position: absolute;
+    line-height: 100px;
+}
+.block-3{
+    width: 34%;
+    height: 50%;
+    background-color: #45a245;
+    left: 33%;
+    top: 50%;
+    position: absolute;
+    line-height: 100px;
+}
+.block-4{
+    width: 34%;
+    height: 50%;
+    background-color: #c1fdc1;
+    left: 33%;
+    top: 0;
+    position: absolute;
+    line-height: 100px;
+}
+.block-5{
+    width: 33%;
+    height: 50%;
+    background-color: #c1fdc1;
+    left: 67%;
+    top: 50%;
+    position: absolute;
+    line-height: 100px;
+}
+.block-6{
+    width: 33%;
+    height: 50%;
+    background-color: #45a245;
+    left: 67%;
+    top: 0;
+    position: absolute;
+    line-height: 100px;
 }
     
 </style>
